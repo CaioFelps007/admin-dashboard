b980a20cab87d9e64084cc9b1348730e73eb1dbd
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-07-27 21:46:49 -0300

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import 
+{ BsGrid1X2Fill, BsFillArchiveFill, BsPeopleFill, 
+  BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
+ from 'react-icons/bs'
+
+ import { MdAttachMoney } from "react-icons/md";
+
+function Sidebar({openSidebarToggle, OpenSidebar}) {
+  return (
+    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
+        <div className='sidebar-title'>
+            <div className='sidebar-brand'>
+              Venturo
+            </div>
+            <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+        </div>
+
+        <ul className='sidebar-list'>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsGrid1X2Fill className='icon'/> Dashboard
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillArchiveFill className='icon'/> Estoque
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                <MdAttachMoney className='icon'/> Financeiro
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsPeopleFill className='icon'/> Recursos Humanos
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsListCheck className='icon'/> Tarefas
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsMenuButtonWideFill className='icon'/> Logs
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillGearFill className='icon'/> Configurações
+                </a>
+            </li>
+        </ul>
+    </aside>
+  )
+}
+
+export default Sidebar
